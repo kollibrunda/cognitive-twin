e973046511271b46dad95ad9da69d7db91f36fdc
Brunda <22brunda@gmail.com>
2026-05-31 17:02:18 +0530

    fix cors

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ create_tables()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://cognitive-twin-gamma.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
